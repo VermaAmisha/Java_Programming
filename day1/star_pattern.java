22c447d3ec266723f79e0ed9af5409baa02902ce
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-02 22:55:12 +0530

    A program to print star pattern.

---
+++ day1/star_pattern.java
@@ -0,0 +1,7 @@
+
+class star_pattern { //can alsobe written as "public class star_pattern"
+    public static void main(String[] args) {
+        // output
+        System.out.print("*\n* *\n* * *\n* * * *\n* * * * *");
+    }
+}
