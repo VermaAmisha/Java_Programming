916d963cfcac39013241f8ebb6a7b128c48e0965
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-03 15:14:05 +0530

    A program to multiply two numbers.

---
+++ day2/multiply.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class multiply {
+    public static void main(String args[]) {
+        int a = 96;
+        int b = 52;
+        int mul = a * b;
+        System.out.print(mul);
+    }
+}
