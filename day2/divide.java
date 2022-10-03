2fc7b558975b4c6a436619e820d5fe466f3dd11b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-03 15:14:32 +0530

    a program to divide two numbers.

---
+++ day2/divide.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class divide {
+    public static void main(String args[]) {
+        int a = 2;
+        int b = 2;
+        int div = a / b;
+        System.out.print(div);
+    }
+}
