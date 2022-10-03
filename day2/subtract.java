ed7b8d6dd861ea0aee766cc35cd0d64e1a9b4380
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-03 15:13:34 +0530

    A program to subtract two numbers.

---
+++ day2/subtract.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class subtract {
+    public static void main(String args[]) {
+        int a = 26;
+        int b = 15;
+        int sub = a - b;
+        System.out.print(sub);
+    }
+}
