05c1527c7a94ff8133cd84f1547f7c80d502a2a6
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-06 23:14:35 +0530

    A program using for loop to print hello world thrice

---
+++ day4/for_loop.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class for_loop {
+    public static void main(String args[]) {
+        for(int counter = 0 ; counter < 5 ; counter = counter + 1) {
+            System.out.println("Hello World");
+
+        }
+    }
+}
