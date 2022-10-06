fbcd284ea3bca2d027c5e33384b8792f58b77265
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-06 23:15:41 +0530

    A program using for loop to print numbers in rows.

---
+++ day4/for_loop_2.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class for_loop_2 {
+    public static void main(String args[]) {
+        for(int counter = 0 ; counter < 11 ; counter++) {
+        System.out.println(counter);
+
+        // // To print in same line like - 0 1 2 3....10
+        // System.out.print(counter + " ");
+        }
+    }
+}
