dd11dfc9022bd320f7243155be18f058d5b835e8
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-11 22:21:18 +0530

    A program to print 180 degree inverted half pyramid.

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,18 @@
+package day8;
+
+public class inverted_half_pyramid {
+    public static void main(String args[]) {
+        int j = 5 ;
+        for(int i = 1 ; i <= j ; i++) {
+            for(int n = 1 ; n <= j - i ; n++) {
+                System.out.print(" ");
+            }
+
+            for(int k = 1 ; k <= i ; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
