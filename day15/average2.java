9f2d3c58b21b35ce37184241b6e5f486c0536394
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-24 23:50:49 +0530

    A program to find average of three numbers.
    Without creating a function.

---
+++ day15/average2.java
@@ -0,0 +1,16 @@
+package day15;
+import java.util.*;
+
+public class average2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        sc.close();
+    
+        int average = (a + b + c) / 3;
+        System.out.println(average);
+    }   
+}
+     
