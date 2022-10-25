43f57f92a8cfd159c1f5ff1130a05110f4312b9c
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-10-25 23:47:20 +0530

    A program to find the greater number of the two.

---
+++ day16/greatNumber.java
@@ -0,0 +1,23 @@
+package day16;
+import java.util.*;
+
+public class maxValue {
+
+    public static int greaterValue(int x , int y) {
+        if(x > y) {
+            return x;
+        }
+        else {
+            return y;
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value 'x': ");
+        int x = sc.nextInt();
+        System.out.print("Enter another value 'y': ");
+        int y = sc.nextInt();
+        System.out.println("The greater value is " + greaterValue(x , y));
+        sc.close();
+    }
+}
