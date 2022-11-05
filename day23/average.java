614233ce22de3e775fef58168538cefd13736a31
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-11-05 17:51:59 +0530

    A program to find the average of 3 numbers.

---
+++ day23/average.java
@@ -0,0 +1,17 @@
+package day23;
+import java.util.*;
+
+public class average {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        float a = sc.nextFloat();
+        System.out.print("Enter the second number: ");
+        float b = sc.nextFloat();
+        System.out.print("Enter the third number: ");
+        float c = sc.nextFloat();
+        float avg = (a + b + c) / 3;
+        System.out.println("The average of the three numbers are: " + avg);
+        sc.close();
+    }
+}
