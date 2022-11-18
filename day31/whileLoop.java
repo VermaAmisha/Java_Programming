4b26ff1cbbaae24dbfe51f15e653b099855c52b3
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-11-18 14:12:24 +0530

    A program to print name 10 times using while loop.

---
+++ day31/whileLoop.java
@@ -0,0 +1,12 @@
+package day31;
+
+public class whileLoop {
+    public static void main(String args[]) {
+        int counter = 0;
+        while(counter < 10) {
+            System.out.println("Amisha Verma");
+            counter++;
+        }
+        System.out.println("Printed name 10 times.");
+    }
+}
