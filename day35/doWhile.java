6538360fabbf1d08c32e5891691d9fe30869027e
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-11-25 23:35:15 +0530

    A program to understand do while loop.

---
+++ day35/doWhile.java
@@ -0,0 +1,13 @@
+package day35;
+
+public class doWhile {
+    public static void main(String args[]) {
+        int i = 1;
+        do {
+            System.out.println("Hello World");
+            i++;
+        }
+        while(i <= 10);
+        System.out.println("'Hello world' printed 10 times");
+    }
+}
