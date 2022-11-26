1f39d3102882a933477a8da22b3ec267307f7911
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-11-26 23:56:42 +0530

    A program to print name using do while loop.

---
+++ day35/doWhile2.java
@@ -0,0 +1,13 @@
+package day35;
+
+public class doWhile2 {
+    public static void main(String args[]) {
+        int i = 1;
+        do {
+            System.out.println("Amisha Verma");
+            i++;
+        }
+        while(i <= 50);
+        System.out.println("'Amisha Verma' printed 10 times");
+    }
+}
