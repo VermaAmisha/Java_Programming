df1dbd35dd837808322297a6b3413e2627e1d681
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-11-27 23:58:55 +0530

    A do while loop program.

---
+++ day36/doWhile3.java
@@ -0,0 +1,18 @@
+package day36;
+import java.util.*;
+
+public class doWhile {
+    public static void main(String args[]) {
+        int i = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the end of the series: ");
+        int j = sc.nextInt();
+        do {
+            System.out.println("I am Learning Java!");
+            i++;
+        }
+        while(i <= j);
+        System.out.println("'Hello world' printed 10 times");
+        sc.close();
+    }
+}
