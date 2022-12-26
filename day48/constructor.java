59daaa873919bd241d68a3b116a2dc0c4a54114b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-12-26 23:20:03 +0530

    A program to understand constructor concept.

---
+++ day48/constructor.java
@@ -0,0 +1,15 @@
+package day48;
+
+public class constructor {
+    public static void main(String args[]) {
+        Student s1 = new Student();
+    }
+}
+class Student {
+    String name;
+    int roll;
+
+    Student() {
+        System.out.println("Constructor has been called...");
+    }
+}
