a4d3c972e03c06095ad76209a6faae1d97218ef0
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2023-01-10 23:36:31 +0530

    A problem to find out Simple interest.

--- day20/SimpleInterest.java
+++ day20/SimpleInterest.java
@@ -1,7 +1,7 @@
 package day20;
 import java.util.*;
 
-public class simpleInterest {
+public class SimpleInterest {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the principal(in Rs.): ");
