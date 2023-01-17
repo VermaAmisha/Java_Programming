2fca7199739eb146a5565ba4fe9bc0e6da0fbdc2
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2023-01-17 23:10:05 +0530

    A program to understand return statements.

---
+++ day51/ReturnStatement.java
@@ -0,0 +1,13 @@
+package day51;
+
+public class ReturnStatement {
+    public static void main(String args[]) {
+        ReturnStatement r = new ReturnStatement();
+        int sum = r.addition(10, 20); //addition() method is called
+        System.out.println("Sum = " + sum);
+    }
+    int addition(int a , int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
