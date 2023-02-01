5b4ea1699d72eaa69eccb31931817b7d8360ec03
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2023-02-01 23:29:04 +0530

    A program to do string concatenation.

---
+++ day59/StringConcatenation.java
@@ -0,0 +1,9 @@
+package day59;
+public class StringConcatenation {
+    public static void main(String args[]) {
+        String firstName = "Amisha";
+        String lastName = "Verma";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
