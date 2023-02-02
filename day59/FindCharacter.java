0807cdac60881a4bd6cf1e8386cc71b2e6ef08b5
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2023-02-02 23:27:16 +0530

    A program to find character using chatAt method.

---
+++ day59/FindCharacter.java
@@ -0,0 +1,11 @@
+package day59;
+
+public class FindCharacter {
+    public static void main(String args[]) {
+        String firstName = "Amisha";
+        String lastName = "Verma";
+        String fullName = firstName + " " + lastName;
+        // using "charAt" keyword to find any character
+        System.out.println(fullName.chatAt(0));
+    }
+}
