b4dc13f9bf19c67cac99ccc3a48eb2fb8034a752
Amisha Verma <91117662+VermaAmisha@users.noreply.github.com>
2023-11-01 23:50:25 +0530

    Create BinaryTree.java

---
+++ day86/BinaryTree.java
@@ -0,0 +1,61 @@
+class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+public class BinaryTree {
+    TreeNode root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(int data) {
+        root = insertRec(root, data);
+    }
+
+    private TreeNode insertRec(TreeNode root, int data) {
+        if (root == null) {
+            root = new TreeNode(data);
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = insertRec(root.left, data);
+        } else if (data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+
+        return root;
+    }
+
+    public void inOrderTraversal(TreeNode node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inOrderTraversal(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.insert(5);
+        tree.insert(3);
+        tree.insert(8);
+        tree.insert(2);
+        tree.insert(4);
+        tree.insert(7);
+        tree.insert(9);
+
+        System.out.println("Inorder Traversal of the Binary Tree:");
+        tree.inOrderTraversal(tree.root);
+    }
+}
